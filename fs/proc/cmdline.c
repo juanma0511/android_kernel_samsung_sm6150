bb5bc66da7514414765333fa6dd9139b07c5c4d4
tisenu100 <tisenu100@github.com>
2025-01-30 17:19:36 +0200

    proc: introduce a bootloader argument spoofer

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -15,6 +15,35 @@ static int cmdline_proc_open(struct inode *inode, struct file *file)
 	return single_open(file, cmdline_proc_show, NULL);
 }
 
+#ifdef CONFIG_BOOTLOADER_ARGUMENTS_SPOOF
+static void patch_flag(char *cmd, const char *flag, const char *val)
+{
+	size_t flag_len, val_len;
+	char *start, *end;
+
+	start = strstr(cmd, flag);
+	if (!start)
+		return;
+
+	flag_len = strlen(flag);
+	val_len = strlen(val);
+	end = start + flag_len + strcspn(start + flag_len, " ");
+	memmove(start + flag_len + val_len, end, strlen(end) + 1);
+	memcpy(start + flag_len, val, val_len);
+}
+
+static void patch_bootloader_arguments(char *cmd)
+{
+	patch_flag(cmd, "androidboot.verifiedbootstate=", "green");
+	patch_flag(cmd, "androidboot.veritymode=", "enforcing");
+	patch_flag(cmd, "androidboot.vbmeta.device_state=", "locked");
+	patch_flag(cmd, "androidboot.warranty_bit=", "0");
+        patch_flag(cmd, "androidboot.em.model=", "SM-S9210 ");
+	patch_flag(cmd, "androidboot.bootloader=", "S9210ZHS4AXK6");
+	patch_flag(cmd, "androidboot.product.model=", "SM-S9210 ");
+}
+#endif
+
 static const struct file_operations cmdline_proc_fops = {
 	.open		= cmdline_proc_open,
 	.read		= seq_read,
